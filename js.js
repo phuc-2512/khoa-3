19686d27c111605b986fe58e9d550c56b4e7c5b9
phuc-2512 <160859584+phuc-2512@users.noreply.github.com>
2024-06-20 20:09:12 +0700

    1

---
+++ js.js
@@ -0,0 +1,33 @@
+// console.log(a)
+// alert('b')
+// prompt('c')
+
+// Các kiểu dữ liệu
+// let: cục bộ 
+// var: toàn bộ
+// const: hằng số
+
+// // Kiểu chuỗi
+// let chuoi1 = 1
+// let chuoi2 = 2
+// let noichuoi = chuoi1+' '+chuoi2
+// console.log(noichuoi);
+
+// // Cách ktr kiểu dữ liệu
+// let number1 = 1;
+// console.log(typeof(number1));
+
+// //Câu lệnh if else
+
+// // let myName = 'khoa'
+ 
+// // if (myName == 'Han') {
+// //     console.log('Xin chào' +myName);
+// // }si
+// // else {
+// //     alert ('không phải tên Hân')
+// // }
+
+ let a = {"học sinh"}
+
+ 
