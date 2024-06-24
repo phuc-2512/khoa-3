85e20d66a5353e7069054fd8d9ecb1ebd20d0c1f
phuc-2512 <160859584+phuc-2512@users.noreply.github.com>
2024-06-24 11:11:31 +0700

    1

--- btap b1/js.js
+++ btap b1/js.js
@@ -1,8 +1,8 @@
 let student = [
     {
-    name: 'NVA',
-    age: 16,
-    course: ['mindx01', 'mindx02', 'mind03']
+        name: 'NVA',
+        age: 16,
+        course: ['mindx01', 'mindx02', 'mind03']
     },
     {
         name: 'NVB',
@@ -15,19 +15,19 @@ let student = [
         course: ['mindx01', 'mindx03', 'mindx04']
     },
     {
-        name:'NVD',
+        name: 'NVD',
         age: 15,
         course: ['mindx01', 'mindx02', 'mindx04']
-    } 
+    }
 ]
 
 console.log(student);
 
 let a = prompt('Độ tuổi cần tìm?')
 
-for (let i = 0; i<student.length; i++) {
-    if(a == student) {
-        alert(student[i])
+for (let i = 0; i < student.length; i++) {
+    if (a == student[i++][i + 1]) {
+        alert(a)
     }
     else {
         alert('Không tìm thấy!')
