0f4d2c0363c0e3b5611ad11739a0c9f82f338fbd
phuc-2512 <160859584+phuc-2512@users.noreply.github.com>
2024-07-10 23:29:56 +0700

    1

---
+++ b2/btap/js.js
@@ -0,0 +1,28 @@
+// const arr1 = [1,2,3,5];
+// const arr2 = [...arr1];
+// const arr3 = [arr1.slice()];
+
+// console.log(arr3);
+
+// var arrA = ['Hello','Xin chào'];
+// var arrB = ['Bonjour', 'Olá'];
+// // var waysToSayHello = [...arrA,...arrB];
+// var waysToSayHello = arrA.concat(arrB);
+
+// console.log(waysToSayHello);
+
+// const arrX = [0,1,2,3,4,5,6,7,8];
+// const arrSquare = [];
+// const arrOdds = [];
+
+// for (const x of arrX){
+//     const square = x ** 2;
+//     arrSquare.push(square);
+//     if(x % 2 !== 0){
+//         arrOdds.push(x);
+//     }
+// }
+
+// console.log(arrSquare);
+// console.log(arrOdds);
+
