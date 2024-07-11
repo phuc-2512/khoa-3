76c5c169bc8b303414a5c97e020efb0454e34d77
phuc-2512 <160859584+phuc-2512@users.noreply.github.com>
2024-07-11 21:04:05 +0700

    1

--- b2/btap/js.js
+++ b2/btap/js.js
@@ -1,15 +1,17 @@
-// const arr1 = [1,2,3,5];
+// const arr1 = [1, 2, 3, 5];
 // const arr2 = [...arr1];
 // const arr3 = [arr1.slice()];
 
+// console.log(arr2);
 // console.log(arr3);
 
 // var arrA = ['Hello','Xin chào'];
 // var arrB = ['Bonjour', 'Olá'];
-// // var waysToSayHello = [...arrA,...arrB];
-// var waysToSayHello = arrA.concat(arrB);
+// var waysToSayHello1 = [...arrA,...arrB];
+// var waysToSayHello2 = arrA.concat(arrB);
 
-// console.log(waysToSayHello);
+// console.log(waysToSayHello1);
+// console.log(waysToSayHello2);
 
 // const arrX = [0,1,2,3,4,5,6,7,8];
 // const arrSquare = [];
@@ -25,4 +27,3 @@
 
 // console.log(arrSquare);
 // console.log(arrOdds);
-
--- b3/js.js
+++ b3/js.js
@@ -1,23 +1,78 @@
-const url = 'https://covid-19-data.p.rapidapi.com/country/code?format=json&code=it';
-const options = {
-    method: 'GET',
-    headers: {
-        'x-rapidapi-key': '7b56229af1msh8cf4f32449cde5ap145397jsn28c7636b10d2',
-        'x-rapidapi-host': 'covid-19-data.p.rapidapi.com'
-    }
-};
+// const url = 'https://covid-19-data.p.rapidapi.com/country/code?format=json&code=it';
+// const options = {
+//     method: 'GET',
+//     headers: {
+//         'x-rapidapi-key': '7b56229af1msh8cf4f32449cde5ap145397jsn28c7636b10d2',
+//         'x-rapidapi-host': 'covid-19-data.p.rapidapi.com'
+//     }
+// };
 
-async function Api() {
-    try {
-        const response = await fetch(url, options);
-        const result = await response.text();
-        var data = JSON.parse(result);
-        console.log(data[0].deaths);
-        console.log(data[0].country);
-        // check kiểu dữ liệu
-        // console.log(typeof(result));
-    } catch (error) {
-        console.error(error);
-    }
-}
-Api()
+// async function Api() {
+//     try {
+//         const response = await fetch(url, options);
+//         const result = await response.text();
+//         var data = JSON.parse(result);
+//         console.log(data[0].deaths);
+//         console.log(data[0].country);
+//         // check kiểu dữ liệu
+//         // console.log(typeof(result));
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
+// Api()
+
+// obj
+// var info = {
+//     Name: 'abc',
+//     aga: 18,
+//     class: '12A'
+// }
+
+// var copyinfo = Object.assign({},info)
+// console.log(copyinfo);
+
+// var copyinfo2 = Object.assign({},info, {school: 'THCS DA'})
+// console.log(copyinfo2);
+
+// let o1 ={a:1, b:2, c:3}
+// let o2 ={b:3, c:4}
+// let o3 ={c:5}
+
+// var copy = Object.assign({}, o1)
+
+// spread syntax
+// var mid = [3, 4];
+// var arr = [1, 2, ...mid, 5, 6];
+
+// console.log(arr);
+
+// Math
+// var arr = [2, 4, 6, 8, 0];
+// var max = Math.max(...arr) 
+// console.log(max);
+
+// var min = Math.min(...arr);
+// console.log(min);
+
+// string to array
+// var str = 'hello';
+// var arr = [...str]
+// console.log(arr);
+
+// arrow funtion 
+// function a() {
+//     console.log('h');
+// }
+// a()
+
+// b = () => {
+//     console.log('h');
+// }
+// b()
+
+// map()
+const a = [1, 4, 3, 8]
+
+var dtich = lengthArr (canh => canh*canh)
+console.log(dtich);
