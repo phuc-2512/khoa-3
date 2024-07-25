c8f03da721dcc441681ce7fbfecdaaa1e1d195ca
phuc-2512 <160859584+phuc-2512@users.noreply.github.com>
2024-07-25 20:55:33 +0700

    5

---
+++ b5/js.js
@@ -0,0 +1,19 @@
+// // Câu 1
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// let arr_even = arr.filter(x => x % 2 === 0)
+
+// let arr_odd = arr.filter(x => x % 2 !== 0)
+
+// console.log('arr_odd binh phuong la: ', arr_odd.map(x => x ** 2));
+// console.log('arr_even nhan hai la: ', arr_even.map(x => x * 2));
+
+// Câu 2
+let s = 'Hello world'
+
+let arr = s.split(' ')
+
+console.log(arr);
+
+// Câu 3
