619e78bc499dcbb813c206938326184c355ec6db
phuc-2512 <160859584+phuc-2512@users.noreply.github.com>
2024-08-08 19:35:20 +0700

    b

---
+++ b6/login.js
@@ -0,0 +1,47 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCewqE91RZyKmw9dAPjfsCvoq4oZcy_7FU",
+    authDomain: "aioi-38518.firebaseapp.com",
+    projectId: "aioi-38518",
+    storageBucket: "aioi-38518.appspot.com",
+    messagingSenderId: "186472053598",
+    appId: "1:186472053598:web:1fe651af0abc64135c6dcf",
+    measurementId: "G-ENWZFC3V9N"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+//submit button
+
+const submit = document.getElementById('submit');
+submit.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    //inputs
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed up 
+            const user = userCredential.user;
+            alert('Đăng ký...')
+            window.location.href = './index1.html'
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            alert(errorMessage)
+            // ..
+        });
+});
